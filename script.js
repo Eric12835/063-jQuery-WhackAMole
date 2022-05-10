8c79b1dd6a19618a216dd687f494e2a37225566a
Eric12835 <105296735+Eric12835@users.noreply.github.com>
2022-05-10 10:04:50 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+$(".whack1").click(function(){
+    $(".mole1").hide();
+});
+    
+$(".whack2").click(function(){
+    $("mole-two").hide();
+});
+    
+$("INCORRECT").click(function(){
+    $("INCORRECT").hide();
+});
+    
+$("INCORRECT").click(function(){
+    $("INCORRECT").show();
+});
